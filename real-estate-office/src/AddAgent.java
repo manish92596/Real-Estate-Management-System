d9503e1cd751249443ecbd37d79dd4219cebf16d
manish92596 <manishkumar92596@gmail.com>
2023-07-19 01:42:28 +0530

    first commit

--- real-estate-office/src/AddAgent.java
+++ real-estate-office/src/AddAgent.java
@@ -122,7 +122,7 @@ public class AddAgent extends JFrame {
 	 */
 	public AddAgent() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 450);
+		setBounds(100, 100, 450, 499);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
